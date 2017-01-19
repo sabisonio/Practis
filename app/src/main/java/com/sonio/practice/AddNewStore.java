8cb20a782b2ba0f917607b9afc55ff3ca35a4281
sonio <364884980@qq.com>
2017-01-19 23:20:47 +0800

    spinner联动

---
+++ app/src/main/java/com/sonio/practice/AddNewStore.java
@@ -0,0 +1,15 @@
+package com.sonio.practice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by admin on 2017/1/19.
+ */
+
+public class AddNewStore extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addnewstore);
+    }
+}
